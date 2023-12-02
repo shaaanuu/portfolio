4e5940b334ec52572f966e67add7171df2e18539
shaaanuu <monshan50@gmail.com>
2023-12-02 15:01:03 +0530

    added SVG for the heroSection

--- src/heroTailSec/HeroTailSec.jsx
+++ src/heroTailSec/HeroTailSec.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import "./HeroTailSec.css"
+import React from "react";
+import "./HeroTailSec.css";
 
 export const HeroTailSec = () => {
   return (
-   <div className="HeroTailSec"></div>
-  )
-}
+    <div className="HeroTailSec">
+      <p>
+        Crafting unified digital experiences across platforms, ensuring a warm
+        welcome into a world of seamless connectivity.
+      </p>
+    </div>
+  );
+};
