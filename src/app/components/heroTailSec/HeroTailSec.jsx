6588a72ace67fc3c9298bb566f0e7e06ac8bc598
shaaanuu <monshan50@gmail.com>
2025-03-11 16:14:19 +0530

    update: heroTailSection

--- src/app/components/heroTailSec/HeroTailSec.jsx
+++ src/app/components/heroTailSec/HeroTailSec.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import "./HeroTailSec.scss";
 
 export const HeroTailSec = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  useEffect(() => {
+    const cursor = document.querySelector(".cursor");
+    if (cursor && isHovering) {
+      cursor.classList.add("hero-tail-hover");
+    }
+
+    return () => {
+      const cursor = document.querySelector(".cursor");
+      if (cursor) {
+        cursor.classList.remove("hero-tail-hover");
+      }
+    };
+  }, [isHovering]);
+
   return (
     <div className="HeroTailSec" id="about">
-      <h1>
-        Crafting unified digital experiences across platforms, ensuring a warm
-        welcome into a world of seamless connectivity.
+      <h1
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+        className={isHovering ? "text-hover" : ""}
+      >
+        I blend design with code to craft experiences that breathe life into
+        digital spaces
       </h1>
     </div>
   );
