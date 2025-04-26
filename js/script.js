d34a2f2d0842f42c6ea5452f10f4902bdddb8b9c
shaaanuu <monshan50@gmail.com>
2025-04-26 20:20:40 +0530

    add: hero section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ const a = document.getElementById("menu-btn");
 a.addEventListener("click", () => {
   console.log("Need to implement the menu for small screens....");
 });
+
+// Generate the random glyphs in hero
+"ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJK".split("").forEach((letter) => {
+  const div = document.createElement("div");
+  div.className = "letter";
+  div.textContent = letter;
+  div.style.left = `${Math.random() * (window.innerWidth - 100)}px`;
+  div.style.top = `${Math.random() * 100}%`;
+  document.querySelector(".hero").appendChild(div);
+});
